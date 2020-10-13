d8fb00d65ac506a5c10274658a5318fe152d823e
Leefrost <evertodante@gmail.com>
2020-10-13 21:18:00 +0300

    dev: Initial commit

---
+++ source/CleanApiTemplate.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CleanApiTemplate.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ source/CleanApiTemplate.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CleanApiTemplate.Domain
+{
+    public class Class1
+    {
+    }
+}
